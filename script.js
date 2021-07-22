40e4ec2963800c4be8b55e6249f7d3dbbd63ecb7
Анастасия <anastasia@MacBook-Pro-Anastasia.local>
2021-07-22 12:42:47 +0200

    com

---
+++ script.js
@@ -0,0 +1,15 @@
+const weight = prompt('What is your weight?'); 
+const height = prompt('What is your height?'); 
+const bodyMassIndex = weight / Math.pow(height,2); /* математическая конструкция где мы делим вес на рост в квадрате отсюда 2 после запятой - это квадрат */ 
+
+const areYouOverweight = bodyMassIndex >=25; 
+
+console.log (bodyMassIndex);
+console.log (areYouOverweight);
+
+alert ('Are you overweight?' + areYouOverweight); 
+
+
+
+
++
